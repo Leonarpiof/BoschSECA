f7c0b87e71f99ba2888065f5e7cd46ede121b6e5
Leon Arpio Fernandez <ie702086@iteso.mx>
2019-03-24 19:40:21 -0600

    Add CAN_Init function

--- can_driver.c
+++ can_driver.c
@@ -13,3 +13,150 @@
  	 \date 	27/03/2019
  */
 
+#include "can_driver.h"
+#include "S32K144.h"
+
+#define INIT_VAL				(0)
+#define MAX_MSG_BUFFERS			(128)
+#define MAX_FILTER_BUFFERS		(16)
+#define CHECK_ALL_MESSAGES		(0xFFFFFFFF)
+
+#define MSG_BUF_SIZE			(4)
+
+#define GLOBAL_ACCEPTANCE_MASK	(0x1FFFFFFF)
+#define ENABLE_RX_BUFF			(0x04000000)
+
+#define PARAM_OK				(0)
+#define PARAM_NOT_OK			(1)
+
+return_codes_t CAN_Init(CAN_init_t config)
+{
+	/** Coutner to clean the RAM*/
+	uint8_t counter;
+	uint8_t param_ok = PARAM_OK;
+
+	/** Default return value*/
+	return_codes_t retval = CAN_success;
+
+	if(config.speed != speed_50kbps ||
+			config.speed != speed_100kbps ||
+			config.speed != speed_250kbps ||
+			config.speed != speed_500kbps)
+	{
+		param_ok = PARAM_NOT_OK;
+	}
+
+
+	if(CAN_0 == config.alternative && PARAM_OK == param_ok)
+	{
+		/** Enables the peripheral clock*/
+		PCC->PCCn[PCC_FlexCAN0_INDEX] |= PCC_PCCn_CGC_MASK;
+
+		/** Disables the Clock*/
+		CAN0->MCR |= CAN_MCR_MDIS_MASK;
+		/** Sets the clock source to the oscillator clock*/
+		//TODO: Funciona con peripheral clock?
+		CAN0->CTRL1 &= (~CAN_CTRL1_CLKSRC_MASK);
+		/** Enables the module*/
+		CAN0->MCR &= (~CAN_MCR_MDIS_MASK);
+
+		/** Waits for the module to enter freeze mode*/
+		while(!((CAN0->MCR & CAN_MCR_FRZACK_MASK) >> CAN_MCR_FRZACK_SHIFT));
+
+		//TODO: Probar todas las velocidades
+		/** Configures the speed, and other parameters*/
+		CAN0->CTRL1 |= config.speed;
+
+		for(counter = INIT_VAL ; MAX_MSG_BUFFERS > counter ; counter ++)
+		{
+			CAN0->RAMn[counter] = INIT_VAL;
+
+			if(MAX_FILTER_BUFFERS > counter)
+			{
+				CAN0->RXIMR[counter] = CHECK_ALL_MESSAGES;
+			}
+		}
+
+		CAN0->RXMGMASK = GLOBAL_ACCEPTANCE_MASK;
+
+		//TODO: ¿Debe estar esto aqui?¿Cómo configurar los buffers de recepción de manera más dinámica?
+		/************************************************************************************************/
+		CAN0->RAMn[ 4*MSG_BUF_SIZE + 0] = ENABLE_RX_BUFF; /* Msg Buf 4, word 0: Enable for reception */
+
+
+	#ifdef NODE_A                                   /* Node A receives msg with std ID 0x511 */
+		CAN0->RAMn[ 4*MSG_BUF_SIZE + 1] = 0x14440000; /* Msg Buf 4, word 1: Standard ID = 0x111 */
+	#else                                           /* Node B to receive msg with std ID 0x555 */
+		CAN0->RAMn[ 4*MSG_BUF_SIZE + 1] = 0x15540000; /* Msg Buf 4, word 1: Standard ID = 0x555 */
+	#endif
+		/***********************************************************************************************/
+
+		/** CAN FD not used*/
+		CAN0->MCR |= 0x0000001F;       /* Negate FlexCAN 1 halt state for 32 MBs */
+
+		/** Waits for the module to exit freeze mode*/
+		while ((CAN0->MCR && CAN_MCR_FRZACK_MASK) >> CAN_MCR_FRZACK_SHIFT);
+		/** Waits for the module to be ready*/
+		while ((CAN0->MCR && CAN_MCR_NOTRDY_MASK) >> CAN_MCR_NOTRDY_SHIFT);
+	}
+
+	else if(CAN_1 == config.alternative && PARAM_OK == param_ok)
+	{
+		/** Enables the peripheral clock*/
+		PCC->PCCn[PCC_FlexCAN1_INDEX] |= PCC_PCCn_CGC_MASK;
+
+		/** Disables the module*/
+		CAN1->MCR |= CAN_MCR_MDIS_MASK;
+		/** Sets the clock source to the oscillator clock*/
+		//TODO: Funciona con peripheral clock?
+		CAN1->CTRL1 &= (~CAN_CTRL1_CLKSRC_MASK);
+		/** Enables the module*/
+		CAN1->MCR &= (~CAN_MCR_MDIS_MASK);
+
+		/** Waits for the module to enter freeze mode*/
+		while(!((CAN1->MCR & CAN_MCR_FRZACK_MASK) >> CAN_MCR_FRZACK_SHIFT));
+
+		//TODO: Probar todas las velocidades
+		/** Configures the speed, and other parameters*/
+		CAN1->CTRL1 |= config.speed;
+
+		for(counter = INIT_VAL ; MAX_MSG_BUFFERS > counter ; counter ++)
+		{
+			CAN1->RAMn[counter] = INIT_VAL;
+
+			if(MAX_FILTER_BUFFERS > counter)
+			{
+				CAN1->RXIMR[counter] = CHECK_ALL_MESSAGES;
+			}
+		}
+
+		CAN1->RXMGMASK = GLOBAL_ACCEPTANCE_MASK;
+
+		//TODO: ¿Debe estar esto aqui?¿Cómo configurar los buffers de recepción de manera más dinámica?
+		/************************************************************************************************/
+		CAN1->RAMn[ 4*MSG_BUF_SIZE + 0] = ENABLE_RX_BUFF; /* Msg Buf 4, word 0: Enable for reception */
+
+
+	#ifdef NODE_A                                   /* Node A receives msg with std ID 0x511 */
+		CAN1->RAMn[ 4*MSG_BUF_SIZE + 1] = 0x14440000; /* Msg Buf 4, word 1: Standard ID = 0x111 */
+	#else                                           /* Node B to receive msg with std ID 0x555 */
+		CAN1->RAMn[ 4*MSG_BUF_SIZE + 1] = 0x15540000; /* Msg Buf 4, word 1: Standard ID = 0x555 */
+	#endif
+		/***********************************************************************************************/
+
+		/** CAN FD not used*/
+		CAN1->MCR |= 0x0000001F;       /* Negate FlexCAN 1 halt state for 32 MBs */
+
+		/** Waits for the module to exit freeze mode*/
+		while ((CAN1->MCR && CAN_MCR_FRZACK_MASK) >> CAN_MCR_FRZACK_SHIFT);
+		/** Waits for the module to be ready*/
+		while ((CAN1->MCR && CAN_MCR_NOTRDY_MASK) >> CAN_MCR_NOTRDY_SHIFT);
+	}
+
+	else
+	{
+		retval = CAN_parameter_error;
+	}
+
+	return retval;
+}
--- can_driver.h
+++ can_driver.h
@@ -16,6 +16,37 @@
 #ifndef CAN_DRIVER_H_
 #define CAN_DRIVER_H_
 
+#define CAN_CTRL1_SPEED_500KBPS			(0x00DB0006)
+#define CAN_CTRL1_SPEED_250KBPS			(0x01DB0006)
+#define CAN_CTRL1_SPEED_100KBPS			(0x04DB0006)
+#define CAN_CTRL1_SPEED_50KBPS			(0x09DB0006)
 
+typedef enum
+{
+	CAN_success,
+	CAN_parameter_error
+}return_codes_t;
+
+typedef enum
+{
+	CAN_0,
+	CAN_1
+}CAN_alternative_t;
+
+typedef enum
+{
+	speed_500kbps = CAN_CTRL1_SPEED_500KBPS,
+	speed_250kbps = CAN_CTRL1_SPEED_250KBPS,
+	speed_100kbps = CAN_CTRL1_SPEED_100KBPS,
+	speed_50kbps = CAN_CTRL1_SPEED_50KBPS,
+}CAN_speed_t;
+
+typedef struct
+{
+	CAN_alternative_t alternative;
+	CAN_speed_t speed;
+}CAN_init_t;
+
+return_codes_t CAN_Init(CAN_init_t config);
 
 #endif /* CAN_DRIVER_H_ */
